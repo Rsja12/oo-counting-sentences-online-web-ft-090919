341ad8b0acb34086800d95c5f7ba30253c86235c
Alfonso Socarras <asocarras10@gmail.com>
2019-09-18 19:55:49 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -28,7 +28,7 @@ class String
   end
 
   def count_sentences
-    self.split(/[.!?]/).delete_if {|x| x.empty?}.count 
+    self.split(/[.!?]/).delete_if {|word| word.empty?}.count 
     # binding.pry
   end
 end
